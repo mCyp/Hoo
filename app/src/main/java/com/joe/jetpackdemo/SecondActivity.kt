b03f1255e1c9932c0b3c03d5abb2d5dee6abb64d
mCyp <200522649@qq.com>
2019-05-16 08:59:22 +0800

    update navigation

--- app/src/main/java/com/joe/jetpackdemo/SecondActivity.kt
+++ app/src/main/java/com/joe/jetpackdemo/SecondActivity.kt
@@ -1,39 +1,66 @@
 package com.joe.jetpackdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration:AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val host:NavHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
         val navController = host.navController
 
-        setupBottomNavMenu(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
+        setActionBar(navController,appBarConfiguration)
+
+        setupBottomNavMenu(navController)
+    }
+
+    /**
+     * 设置ActionBar
+     */
+    private fun setActionBar(navController: NavController,appBarConfig: AppBarConfiguration){
+        setupActionBarWithNavController(navController,appBarConfig)
     }
 
     private fun setupBottomNavMenu(navController: NavController){
         val bottomNavigate:BottomNavigationView = findViewById(R.id.navigation_view)
-        //bottomNavigate.setupWithNavController(navController)
-        NavigationUI.setupWithNavController(bottomNavigate,navController)
-        /*bottomNavigate.setOnNavigationItemSelectedListener {
+        bottomNavigate.setupWithNavController(navController)
+        //NavigationUI.setupWithNavController(bottomNavigate,navController)
+       /* bottomNavigate.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.nav_home -> Navigation.findNavController(bottomNavigate).navigate(R.id.navHostFragment)
-                R.id.nav_trans -> Navigation.findNavController(bottomNavigate).navigate(R.id.setttingFragment)
+                R.id.nav_home -> navController.navigate(R.id.navHostFragment)
+                R.id.nav_trans -> navController.navigate(R.id.setttingFragment)
             }
             true
         }*/
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp() = findNavController(this, R.id.my_nav_host_fragment).navigateUp()
---
+++ app/src/main/java/com/joe/jetpackdemo/ui/provider/DeepLinkAppWidgetProvider.kt
@@ -0,0 +1,30 @@
+package com.joe.jetpackdemo.ui.provider
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
+import com.joe.jetpackdemo.R
+
+class DeepLinkAppWidgetProvider: AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        val remoteView = RemoteViews(context.packageName,R.layout.deep_link_appwidget)
+
+        val args = Bundle()
+        args.putString("myarg","from widget")
+
+        val pendIntent = NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.mobile_navigation)
+            .setDestination(R.id.deeplink_dest)
+            .setArguments(args)
+            .createPendingIntent()
+
+        remoteView.setOnClickPendingIntent(R.id.deep_link_button,pendIntent)
+        appWidgetManager.updateAppWidget(appWidgetIds,remoteView)
+    }
+}
